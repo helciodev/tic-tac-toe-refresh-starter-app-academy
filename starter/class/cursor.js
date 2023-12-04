b288ef474abac942e26eb35680d59a00899dd1d3
helcio <h.andre@giap.pl>
2023-12-04 13:34:14 +0100

    Complete filling the cursor movements

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -1,7 +1,6 @@
 const Screen = require("./screen");
 
 class Cursor {
-
   constructor(numRows, numCols) {
     this.numRows = numRows;
     this.numCols = numCols;
@@ -9,9 +8,8 @@ class Cursor {
     this.row = 0;
     this.col = 0;
 
-    this.gridColor = 'black';
-    this.cursorColor = 'yellow';
-
+    this.gridColor = "black";
+    this.cursorColor = "yellow";
   }
 
   resetBackgroundColor() {
@@ -23,22 +21,28 @@ class Cursor {
   }
 
   up() {
-    // Move cursor up
+    if (this.row >= 1) {
+      this.row -= 1;
+    }
   }
 
   down() {
-    // Move cursor down
+    if (this.row < 2) {
+      this.row += 1;
+    }
   }
 
   left() {
-    // Move cursor left
+    if (this.col >= 1) {
+      this.col -= 1;
+    }
   }
 
   right() {
-    // Move cursor right
+    if (this.col < 2) {
+      this.col += 1;
+    }
   }
-
 }
-
 
 module.exports = Cursor;
